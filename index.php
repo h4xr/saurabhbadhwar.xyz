788ed6b5cf1ef59e215bc121c3d98770d71273af
Saurabh <sbsaurabhbadhwar9@gmail.com>
2015-11-28 19:51:10 +0530

    Add animate.css

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html lang="en-us">
 <head>
     <title>Saurabh Badhwar's Website</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
 </head>
 <body>
-
+<img src="images/logo.png">
 </body>
 </html>
