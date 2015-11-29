0a3a5b9c60f421aa79358a302f3a753cc6cd6641
Saurabh <sbsaurabhbadhwar9@gmail.com>
2015-11-29 12:39:10 +0530

    Let's start

--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 <html lang="en-us">
 <head>
     <title>Saurabh Badhwar's Website</title>
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="css/style.css">
-
 </head>
 <body>
-<img src="images/logo.png">
+    <nav>
+        <div id="menu" class="primary-nav">
+            <ul>
+                <li class="nav-link transition">Home</li>
+                <li class="nav-link transition">About</li>
+                <li class="nav-link transition">Writing</li>
+                <li class="nav-link transition">Work</li>
+                <li class="nav-link transition">Contact</li>
+            </ul>
+        </div>
+    </nav>
+    <header>
+        <div id="logo" class="text-logo">
+            Saurabh Badhwar
+        </div>
+    </header>
 </body>
 </html>
